aae1e5232de90f2db61ec2e7853eec621d71acbc
Dima <rubtherobs@gmail.com>
2022-04-13 10:33:21 +0300

    Lesson_2

---
+++ Урок 2. Практическое задание/task_1/main.py
@@ -0,0 +1,86 @@
+"""
+1. Задание на закрепление знаний по модулю CSV. Написать скрипт,
+осуществляющий выборку определенных данных из файлов info_1.txt, info_2.txt,
+info_3.txt и формирующий новый «отчетный» файл в формате CSV.
+
+Для этого:
+
+Создать функцию get_data(), в которой в цикле осуществляется перебор файлов
+с данными, их открытие и считывание данных. В этой функции из считанных данных
+необходимо с помощью регулярных выражений или другого инструмента извлечь значения параметров
+«Изготовитель системы», «Название ОС», «Код продукта», «Тип системы».
+Значения каждого параметра поместить в соответствующий список. Должно
+получиться четыре списка — например, os_prod_list, os_name_list,
+os_code_list, os_type_list. В этой же функции создать главный список
+для хранения данных отчета — например, main_data — и поместить в него
+названия столбцов отчета в виде списка: «Изготовитель системы»,
+«Название ОС», «Код продукта», «Тип системы». Значения для этих
+столбцов также оформить в виде списка и поместить в файл main_data
+(также для каждого файла);
+
+Создать функцию write_to_csv(), в которую передавать ссылку на CSV-файл.
+В этой функции реализовать получение данных через вызов функции get_data(),
+а также сохранение подготовленных данных в соответствующий CSV-файл;
+
+Пример того, что должно получиться:
+
+Изготовитель системы,Название ОС,Код продукта,Тип системы
+
+1,LENOVO,Windows 7,00971-OEM-1982661-00231,x64-based
+
+2,ACER,Windows 10,00971-OEM-1982661-00231,x64-based
+
+3,DELL,Windows 8.1,00971-OEM-1982661-00231,x86-based
+
+Обязательно проверьте, что у вас получается примерно то же самое.
+
+ПРОШУ ВАС НЕ УДАЛЯТЬ СЛУЖЕБНЫЕ ФАЙЛЫ TXT И ИТОГОВЫЙ ФАЙЛ CSV!!!
+"""
+import csv
+
+def get_data():
+    os_prod_list = []
+    os_name_list = []
+    os_code_list = []
+    os_type_list = []
+    main_data = []
+    headers = ['Изготовитель системы', 'Название ОС', 'Код продукта', 'Тип системы']
+    for i in range(3):
+        with(open(f'info_{i+1}.txt', 'r', encoding='utf-8')) as file:
+            for line in file:
+                if line.rfind('Название ОС') != -1 :
+                    os_name_list.append(((line.split('  ')[-1]).replace('\n', '')).lstrip())
+                if line.rfind('Изготовитель системы') != -1 :
+                    os_prod_list.append(((line.split('  ')[-1]).replace('\n', '')).lstrip())
+                if line.rfind('Код продукта') != -1 :
+                    os_code_list.append(((line.split('  ')[-1]).replace('\n', '')).lstrip())
+                if line.rfind('Тип системы') != -1 :
+                    os_type_list.append(((line.split('  ')[-1]).replace('\n', '')).lstrip())
+    main_data.append(headers)
+
+    j = 1
+    for i in range(0, 3):
+        row_data = []
+        row_data.append(j)
+        row_data.append(os_prod_list[i])
+        row_data.append(os_name_list[i])
+        row_data.append(os_code_list[i])
+        row_data.append(os_type_list[i])
+        main_data.append(row_data)
+        j += 1
+
+
+    return main_data
+
+def write_to_csv(out_file):
+    """Запись данных в csv"""
+
+    main_data = get_data()
+    with open(out_file, 'w', encoding='utf-8') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
+        for row in main_data:
+            writer.writerow(row)
+
+
+write_to_csv('data_report.csv')
+
