fed4e013478385fd0fe31cac99f6ed2d82619c54
Dima <rubtherobs@gmail.com>
2022-05-28 10:34:56 +0300

    Lesson_2.1

--- Урок 3. Практическое задание/server.py
+++ Урок 3. Практическое задание/server.py
@@ -12,7 +12,7 @@ from decorator import log_decorator
 from descrptrs import Port, Host
 import argparse
 from metaclasses import ServerMaker
-
+from server_database import ServerStorage
 
 SERVER_LOGGER = logging.getLogger('server')
 
@@ -29,10 +29,11 @@ class Server(metaclass=ServerMaker):
     port = Port()
     addr = Host()
 
-
-    def __init__(self, listen_address, listen_port):
+    def __init__(self, listen_address, listen_port, database):
         self.addr = listen_address
         self.port = listen_port
+
+        self.database = database
 
         self.clients = []
 
@@ -114,6 +115,8 @@ class Server(metaclass=ServerMaker):
 
             if message[USER][ACCOUNT_NAME] not in self.names.keys():
                 self.names[message[USER][ACCOUNT_NAME]] = client
+                client_ip, client_port = client.getpeername()
+                self.database.user_login(message[USER][ACCOUNT_NAME], client_ip, client_port)
                 send_message(client, RESPONSE_200)
             else:
                 response = RESPONSE_400
@@ -129,6 +132,7 @@ class Server(metaclass=ServerMaker):
             return
 
         elif ACTION in message and message[ACTION] == EXIT and ACCOUNT_NAME in message:
+            self.database.user_logout(message[ACCOUNT_NAME])
             self.clients.remove(self.names[ACCOUNT_NAME])
             self.names[ACCOUNT_NAME].close()
             del self.names[ACCOUNT_NAME]
@@ -140,13 +144,45 @@ class Server(metaclass=ServerMaker):
             send_message(client, response)
             return
 
+def print_help():
+    print('Поддерживаемые комманды:')
+    print('users - список известных пользователей')
+    print('connected - список подключенных пользователей')
+    print('loghist - история входов пользователя')
+    print('exit - завершение работы сервера.')
+    print('help - вывод справки по поддерживаемым командам')
+
 def main():
-    # Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию.
+
     listen_address, listen_port = arg_parser()
+
+    database = ServerStorage()
+
     print(listen_address, listen_port)
-    # Создание экземпляра класса - сервера.
-    server = Server(listen_address, listen_port)
-    server.main_loop()
+    server = Server(listen_address, listen_port, database)
+    server.daemon = True
+    server.start()
+
+    print_help()
+
+    while True:
+        command = input('Введите комманду: ')
+        if command == 'help':
+            print_help()
+        elif command == 'exit':
+            break
+        elif command == 'users':
+            for user in sorted(database.users_list()):
+                print(f'Пользователь {user[0]}, последний вход: {user[1]}')
+        elif command == 'connected':
+            for user in sorted(database.active_users_list()):
+                print(f'Пользователь {user[0]}, подключен: {user[1]}:{user[2]}, время установки соединения: {user[3]}')
+        elif command == 'loghist':
+            name = input('Введите имя пользователя для просмотра истории. Для вывода всей истории, просто нажмите Enter: ')
+            for user in sorted(database.login_history(name)):
+                print(f'Пользователь: {user[0]} время входа: {user[1]}. Вход с: {user[2]}:{user[3]}')
+        else:
+            print('Команда не распознана.')
 
 
 if __name__ == '__main__':
